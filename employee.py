d168c871e10c8da0c17f3e24dc742794ad0a533d
ruman-jagirdar <rumanjagirdar08@gmail.com>
2025-11-15 11:39:24 +0530

    Create employee.py

---
+++ employee.py
@@ -0,0 +1,11 @@
+
+emp_id = 101
+name = "Ruman"
+address= "hubli"
+salary = 50000
+
+print("Employee Details:")
+print("ID:", emp_id)
+print("Name:", name)
+print("adress:", address)
+print("Salary:", salary)
